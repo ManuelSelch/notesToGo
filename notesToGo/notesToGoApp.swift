328d4cbfaf31f0d1b7d64d3ab723e35e259e7783
Manuel Selch <manuel@selch.de>
2025-12-17 18:39:17 +0100

    Initial Commit

---
+++ notesToGo/notesToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesToGoApp.swift
+//  notesToGo
+//
+//  Created by Manuel Selch on 17.12.25.
+//
+
+import SwiftUI
+
+@main
+struct notesToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
